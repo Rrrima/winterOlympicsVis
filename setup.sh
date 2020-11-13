06a1bfd360cccb543c38de12dba91b7c56a3f56f
Rrrima <rimacyn@gmail.com>
2020-11-13 18:27:02 -0500

    fix bugs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
+echo "\
 [general]\n\
-email = \”rimacyn@umich.edu\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"rimacyn@umich.edu\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
